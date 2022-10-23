8871686d05e494dd1eeb540f4f83e353177b41e9
WalkerKim <cuk25president@gmail.com>
2022-10-23 15:56:21 +0900

    first commit

---
+++ src/main/java/kim/figure/site/common/DummyClassForCommitPackageStructure.java
@@ -0,0 +1,9 @@
+package kim.figure.site.common;
+
+public class DummyClassForCommitPackageStructure  {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/test/java/kim/figure/site/common/DummyClassForCommitPackageStructureTest.java
@@ -0,0 +1,11 @@
+package kim.figure.site.common;
+
+
+/**
+ * author         : walker
+ * date           : 2022. 10. 23.
+ * description    :
+ */
+class DummyClassForCommitPackageStructureTest {
+
+}
